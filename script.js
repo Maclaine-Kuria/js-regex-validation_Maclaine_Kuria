79066b4280b967338bfb38d197f5ba60d77affb6
Maclaine-Kuria <69981065+Maclaine-Kuria@users.noreply.github.com>
2025-03-10 14:20:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("Form");
 
+  const firstName = document.getElementById("FirstName");
+  const lastName = document.getElementById("LastName");
+  const phone = document.getElementById("phone");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
